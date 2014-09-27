b336253ea7f1087622bbbf286cdc27a0ab2a58ce
Stephen Done <srdone@gmail.com>
2014-09-27 08:11:26 -0600

    Add comments.

--- lib.py
+++ lib.py
@@ -3,20 +3,26 @@
 import xml.etree.ElementTree as ET
 
 class Lib:
-	def __init__(self):
-		self.lib_xml = open_lib_xml()
+	def __init__(self, filename):
+		'''Takes an xml filename and processes it to allow a Lib
+		to be generated and interacted with.'''
+		self.lib_xml = open_lib_xml(file)
 
 	def set_word(self, id, word):
-		#retrieve the phrase element with a given id from the xml
+		'''Retrieve the phrase element with a given id from the xml
+		and assign a new attribute for the selected word to match the speech_part'''
 		phrase = self.lib_xml.find(".//phrase[@id='" + id + "']")
 		phrase.attrib["word"] = word
 
 	def gen_speech_parts(self):
+		'''A generator that gives each speech_part and the id of the phrase it belongs to.'''
 		for phrase in self.lib_xml.getiterator("phrase"):
 			if phrase.get("speech_part") is not None:
 				yield {"id": phrase.get("id"), "speech_part": phrase.get("speech_part")}
 
 	def gen_story(self):
+		'''A generator that constructs a string for each phrase.
+		Assumes that a word attribute has been selected.'''
 		for line in self.lib_xml.getiterator("phrase"):
 			string = (line.attrib.get("text") if line.attrib.get("text") is not None else "")
 			string = string + (line.attrib.get("word") if line.attrib.get("word") is not None else "")
@@ -24,8 +30,8 @@ class Lib:
 			string = string + " "
 			yield string
 
-def open_lib_xml():
-	filename = input("Enter the location of the lib file you want: ")
+def open_lib_xml(filename):
+	'''Opens an xml file and returns the parsed xml.'''
 	with open(filename, "r") as lib_file:
 		lib_xml = ET.parse(lib_file)
 		return lib_xml
--- lib_cli.py
+++ lib_cli.py
@@ -2,32 +2,42 @@ import lib
 from lib import Lib
 
 def play_game():
+	'Game flow'
 	display_intro()
 	plyr_name = get_player_name()
-	lib = Lib()
+	lib = Lib(get_lib_name())
 	request_words(lib)
 	display_story(lib)
 	if play_again():
 		play_game()
 
 def display_intro():
+	'''Displays the game introduction text.'''
 	print("Welcome to LIB, the fun game to learn parts of speech")
+	print("By entering words to match parst of speech, you produce a wacky story.")
 
 def get_player_name():
+	'Requests the name of the player.'
 	return input("What is your name?: ")
 
-#Ask the user to pick a word for each speech part in the LIB
+def get_lib_name():
+	'''Get the name and relative path of the Lib you want to play.'''
+	return input("Enter the relative path to your lib file.")
+
 def request_words(lib):
+	'Ask the user to pick a word for each speech part in the Lib.'
 	print("Enter words that match each part of speech below: ")
 	for speech_part in lib.gen_speech_parts():
 		word = input(speech_part["speech_part"] + ": ")
 		lib.set_word(speech_part["id"], word)
 
 def display_story(lib):
+	'Displays the filled in phrases, one on each line.'
 	for phrase in lib.gen_story():
 		print(phrase)
 
 def play_again():
+	'Ask the player if they want to play again.'
 	response = input("Do you want to try another LIB? (y/n):")
 	if response.lower() == "y":
 		play_game()
