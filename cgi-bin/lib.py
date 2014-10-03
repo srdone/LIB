07a2d5b387d405771e0a950b02d55b743e6c75e4
Stephen Done <srdone@gmail.com>
2014-10-02 21:36:24 -0600

    adding page to create libs

--- cgi-bin/lib.py
+++ cgi-bin/lib.py
@@ -3,10 +3,13 @@
 import xml.etree.ElementTree as ET
 
 class Lib:
-	def __init__(self, filename):
+	def __init__(self, filename=None):
 		'''Takes an xml filename and processes it to allow a Lib
 		to be generated and interacted with.'''
-		self.lib_xml = open_lib_xml(filename)
+		if filename:
+			self.lib_xml = open_lib_xml(filename)
+		else:
+			self.lib_xml = ET.Element('data')
 
 	def set_word(self, id, word):
 		'''Retrieve the phrase element with a given id from the xml
@@ -25,10 +28,21 @@ class Lib:
 		Assumes that a word attribute has been selected.'''
 		for line in self.lib_xml.getiterator("phrase"):
 			string = (line.attrib.get("text") if line.attrib.get("text") is not None else "")
-			string = string + (line.attrib.get("word") if line.attrib.get("word") is not None else "")
+			if line.attrib.get("word"):
+				string = string + (line.attrib.get("word") if line.attrib.get("word") is not None else "")
+			else:
+				string = string + (line.attrib.get("speech_part") if line.attrib.get("speech_part") is not None else "")
 			string = string + (line.attrib.get("tail") if line.attrib.get("tail") is not None else "")
 			string = (string + line_ending if line.attrib.get('paragraph')=='true' else string) #Create new line if needed.
 			yield string
+
+	def add_phrase(self, id, attributes):
+		'''
+		Adds a phrase to the lib.
+		'''
+		line = SubElement(self.lib_xml, "phrase")
+		for key in attributes.keys():
+			line.set(key, attributes[key])
 
 def open_lib_xml(filename):
 	'''Opens an xml file and returns the parsed xml.'''
---
+++ cgi-bin/lib_create.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+
+import cgi, sys
+import lib
+from lib import Lib
+
+#show std errors in the console
+sys.stderr = sys.stdout
+
+#get data from the previous page
+form = cgi.FieldStorage
+
+#create blank lib
+lib = Lib()
+
+
+
+##Begin webpage
+print('Content-type: text/html\n') #parse form data
+print('<title>Built Lib</title>')
+print('<body>')
+
+#Fill lib in with data from cgi form
+n = 0
+while n < 2:			#(length(form)/4):
+	line_attributes['text' + str(n)] = form['text' + str(n)]
+	print(line_attributes['text' + str(n)])
+	line_attributes['speech-part' + str(n)] = form['speech-part' + str(n)]
+	line_attributes['tail' + str(n)] = form['tail' + str(n)]
+	line_attributes['paragraph' + str(n)] = form['paragraph' + str(n)]
+	lib.add_phrase(n, line_attributes)
+
+#build and print story
+string = ""
+for phrase in lib.gen_story(line_ending='<br>'):
+		string = string + phrase
+print(string)
+
+#close page
+print('</body>')
